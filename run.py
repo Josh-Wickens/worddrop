c4ced6532cfac7cf8b0481b092310358c0a9e853
Josh-Wickens <joshwickens@hotmail.com>
2022-06-03 10:20:22 +0000

    add hangman pictures to constant variables

--- run.py
+++ run.py
@@ -1,5 +1,64 @@
 import random
 
+HANGMANPICS = ['''
+
+   +---+
+   |   |
+       |
+       |
+       |
+       |
+ =========''', '''
+
+   +---+
+   |   |
+   O   |
+       |
+       |
+       |
+ =========''', '''
+
+   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+ =========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|   |
+       |
+       |
+ =========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|\  |
+       |
+       |
+ =========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|\  |
+  /    |
+       |
+ =========''', '''
+
+   +---+
+   |   |
+   O   |
+  /|\  |
+  / \  |
+       |
+
+=========''']
+
 WORDS = ["BALE", "RONALDO", "MESSI", "HESKEY", "BENDTNER", "BENZEMA", "BECKHAM", "SOLSKJAER", "GERRARD", "LAMPARD", "AGUERO", "ZIDANE", "RONALDO", "NEYMAR", "ROONEY", "CHICHARITO", "SALAH", "MARADONA", "INIESTA", "HENRY", "CANTONA", "DROGBA", "LINEKER", "SILVA", "KOMPANY", "MARCELO", "VIDIC", "ERIKSON", "TERRY"]
 
 print("Welcome to Worddrop!\n")
