c1dee3dfacb5b4de25a3b444f2d2f9e3628caef1
Josh-Wickens <joshwickens@hotmail.com>
2022-06-03 09:48:02 +0000

    add random selection function to select a random word from the words variable

--- run.py
+++ run.py
@@ -3,3 +3,7 @@ import random
 WORDS = ["BALE", "RONALDO", "MESSI", "HESKEY", "BENDTNER", "BENZEMA", "BECKHAM", "SOLSKJAER", "GERRARD", "LAMPARD", "AGUERO", "ZIDANE", "RONALDO", "NEYMAR", "ROONEY", "CHICHARITO", "SALAH", "MARADONA", "INIESTA", "HENRY", "CANTONA", "DROGBA", "LINEKER", "SILVA", "KOMPANY", "MARCELO", "VIDIC", "ERIKSON", "TERRY"]
 
 print("Welcome to Worddrop!\n")
+
+randomWord = random.choice(WORDS)
+
+print(randomWord)
