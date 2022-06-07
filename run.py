3f99ae7f532bcd7e21684708fd357424da87cabb
Josh-Wickens <joshwickens@hotmail.com>
2022-06-07 09:23:51 +0000

    fix bug where it wouldn't provide guess letter

--- run.py
+++ run.py
@@ -59,52 +59,62 @@ HANGMANPICS = ['''
 
 =========''']
 
-WORDS = ["BALE", "RONALDO", "MESSI", "HESKEY", "BENDTNER", "BENZEMA", "BECKHAM", "SOLSKJAER",
-"GERRARD", "LAMPARD", "AGUERO", "ZIDANE", "RONALDO", "NEYMAR", "ROONEY", 
-"CHICHARITO", "SALAH", 
-"MARADONA", "INIESTA", "HENRY", "CANTONA", "DROGBA", "LINEKER", "SILVA", 
-"KOMPANY", "MARCELO", 
-"VIDIC", "ERIKSON", "TERRY"]
+WORDS = ["BALE", "RONALDO", "MESSI", "HESKEY", "BENDTNER", "BENZEMA", "BECKHAM", "SOLSKJAER", "GERRARD", "LAMPARD", "AGUERO", "ZIDANE", "RONALDO", "NEYMAR",
+         "ROONEY", "CHICHARITO", "SALAH", "MARADONA", "INIESTA", "HENRY", "CANTONA", "DROGBA", "LINEKER", "SILVA", "KOMPANY", "MARCELO", "VIDIC", "ERIKSON", "TERRY"]
 
 guesses = 0
+
 MAX_WRONG = 7
+
 used_letters = []
+
 wrong_answers = 0
 
 print("\n Before we get started, I need to know your name \n")
 
 username = input("insert name: ")
-
-print(f"Welcome to Worddrop {username}!\n")
-print("Guess the footballers name by typing out a single letter in the console. \n")
+print(f"Welcome to Worddrop {username}!")
+print("\nGuess the footballers name by typing out a single letter in the console. \n")
 print("Too many wrong guesses and the man will hit the gallows! \n")
+print(HANGMANPICS[wrong_answers])
 print("Guess one letter at a time and see if you can save him \n")
 
 random_word = random.choice(WORDS)
-current_word = random_word = "_ " * len(random_word)
+current_word = "_ " * len(random_word)
 
 print(current_word)
 
-while wrong_answers < MAX_WRONG and current_word != random_word:
-  print(HANGMANPICS[wrong_answers])
-  print(f"You have used the following letter: {used_letters}")
-  letter_guess = input("\n guess a letter: ")
-  letter_guess = letter_guess.upper()
-  print("\n You guessed:" + letter_guess)
+while wrong_answers != 6 and current_word != random_word:
+    print(HANGMANPICS[wrong_answers])
+    print(f"You have used the following letter: {used_letters}")
 
-#def show_word():
- # while wrong_answers < MAX_WRONG:
-  #  print(f"you have used the letter: {used_letters}")
-   # print(current_word)
-  #else:
-   # print(HANGMANPICS)
+    letter_guess = input("\n guess a letter: ")
+    letter_guess = letter_guess.upper()
+    print("\n You guessed:" + letter_guess)
 
-#def wrong_guess():
+    while letter_guess in used_letters:
+        print("you have already guessed this letterd")
+    # for letter in used_letters:
+    #   if letter == letter_guess:
+    #      print(
+    #         "you have already guessed this letter. Please try and guess a different letter")
 
-#def correct_guess():
 
-#def lose_game():
+# guess_letter(letter)
+# run_game()
+# def show_word():
+# while wrong_answers < MAX_WRONG:
+#  print(f"you have used the letter: {used_letters}")
+# print(current_word)
+# else:
+# print(HANGMANPICS)
 
-#def win_game():
+# def wrong_guess():
 
-#def play game():
+# def correct_guess():
+
+# def lose_game():
+
+# def win_game():
+
+# def play game():
